ea4314622f009156cf5526d684bcb5f09e0d6055
nazsa <nazsa.9070@gmail.com>
2017-12-16 14:56:03 +0600

    Add files via upload

---
+++ UsefulLinks.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Bookmarks</title>
+</head>
+<body>
+	<h3>Top Movies, Manga</h3>
+	<li><a href="https://animemovieguide.com/movie-guide/">Top Movies</a></li>
+	<li><a href="https://www.reddit.com/r/manga/comments/58t1cl/best_completed_seinen_manga/">Top Seinen Manga</a></li>
+	<p></p>
+    <h3>Detective Conan (after 193)</h3>
+	<li><a href="https://www.xerblade.com/p/detective-conan-important-episode-list.html">What to watch (Or see pdf)</a></li>
+	<li><a href="http://www.animefillerlist.com/shows/detective-conan">List of Fillers (Updated)</a></li>
+	<li><a href="https://www.animeout.xyz/download-detective-conan-all-latest-episodes/">1. Detective Conan (AnimeOut (Preferred but poor quality, follow episode number from URL))</a></li>
+	<li><a href="https://animepahe.com/anime/detective-conan">1. Detective Conan (AnimePahe, good quality but no batch link)</a></li>
+	<li><a href="https://thepiratebay.org/search/detective%20conan%20480p/0/99/0">1. Detective Conan (Torrent, bad quality)</a></li>
+	<li><a href="https://www.animeout.xyz/detective-conan-movies/">2. Movies (AnimeOut, Preferred)</a></li>
+	<li><a href="https://old.animepahe.com/?s=conan+movie">2. Movies (AnimePahe)</a></li>
+	<p></p>
+    <h3>Ace wo Nerae! (after 10, use MegaDownloader (http://localhost:40000/streaming?id=82))</h3>
+	<li><a href="https://mega.nz/#F!t8QT1Aza!IlWldl4_YJU9vaIr2HzECQ">AniDL (Mega)</a></li>
+	<p></p>
+    <h3>LoGH (after 63)</h3>
+	<li><a href="https://mega.nz/#F!xrhkFZZD!k4RqQ21Z69-1BAJKXHCezQ">AnimeKaizoku (Mega)</a></li>
+	<p></p>
+    <h3>HxH (2011, eng dub or dual audio)</h3>
+	<li><a href="https://animekaizoku.com/?s=hunter+x+hunter">AnimeKaizoku</a></li>
+	<p></p>
+	<h3>Some Useful Manga Sites</h3>
+	<li><a href="https://web.facebook.com/notes/%E0%A6%8F%E0%A6%A8%E0%A6%BF%E0%A6%AE%E0%A6%96%E0%A7%8B%E0%A6%B0/%E0%A6%86%E0%A6%AE%E0%A6%BE%E0%A6%B0-%E0%A6%A6%E0%A7%87%E0%A6%96%E0%A6%BE-%E0%A6%B8%E0%A7%87%E0%A6%B0%E0%A6%BE-%E0%A7%A7%E0%A7%A6-%E0%A6%AE%E0%A6%BE%E0%A6%99%E0%A7%8D%E0%A6%97%E0%A6%BE-%E0%A6%86%E0%A6%B0%E0%A7%8D%E0%A6%9F%E0%A6%BF%E0%A6%B8%E0%A7%8D%E0%A6%9F/1541722232507659/">Top Manga (Based on Artwork)</a></li>
+	<li><a href="https://tdxtreme.wordpress.com/">Chihayafuru (TDX)</a></li>
+	<li><a href="http://mangachameleon.com/">Mangachameleon</a></li>
+	<li><a href="https://www.facebook.com/impatientscans/">Akatsuki no Yona (Impatient Scans)</a></li>
+	<li><a href="http://gen.lib.rus.ec/comics/index.php">Library Genesis</a></li>
+	<p></p>
+	<h3>Top 5 Educational Youtube Channels</h3>
+	<li><a href="https://www.youtube.com/user/khanacademy/">1. Khan Academy</a></li>
+	<li><a href="https://www.youtube.com/user/minutephysics/">2. Minutephysics</a></li>
+	<li><a href="https://www.youtube.com/user/AsapSCIENCE">3. AsapScience</a></li>
+	<li><a href="https://www.youtube.com/user/numberphile/">4. Numberphile</a></li>
+	<li><a href="https://www.youtube.com/user/crashcourse/">5. Crashcourse</a></li>
+	<p></p>
+	<h3>Anime OST</h3>
+	<li><a href="http://fvost.net">FVOst</a></li>
+	<li><a href="http://hikarinoakariost.info/anime-ost-list/">Hikari</a></li>
+	<p></p>
+	<h3>Novel/LN/WN</h3>
+	<li><a href="http://armaell-library.net/">Novel/LN/WN</a></li>
+	<li><a href="https://mega.nz/#F!gY1nyB4S!DTR84RL4aB-ZG6oHX-axvw">Mahouka Koukou no Rettousei</a></li>
+	<p></p>
+	<h3>Comics</h3>
+	<li><a href="https://www.facebook.com/notes/792862750726500/%E0%A6%95%E0%A6%AE%E0%A6%BF%E0%A6%95%E0%A6%B8%20%E0%A6%8F%E0%A6%B0%20%E0%A6%B2%E0%A6%BF%E0%A6%99%E0%A7%8D%E0%A6%95%20/1128316027181169/">BanglaPdf</a></li>
+	<li><a href="http://getcomics.info/">English Comics</a></li>
+	<li><a href="http://gen.lib.rus.ec/comics/index.php">Library Genesis</a></li>
+	<p></p>
+	<h3>IMDB top 250</h3>
+	<li><a href="https://www.imdb.com/chart/top-english-movies">IMDB top 250</a></li>
+	<p></p>
+	<h3>Higher Study or Job Links</h3>
+	<li><a href="http://connect.bsadd.org/ambassador_2017.html">1. Some Alumni Job Places</a></li>
+    <li><a href="http://jobtestpaper.com/">2. MCQ (1)</a></li>
+	<li><a href="http://www.examveda.com/">3. IT MCQ (1)</a></li>
+	<li><a href="http://mcqquestions.com">4. IT MCQ (2)</a></li>
+	<p></p>
+	<h3>Anime Subtitles</h3>
+	<li><a href="http://subs.com.ru/index.php?e=search">Subtitles</a></li>
+	<p></p>
+</body>
+</html>
